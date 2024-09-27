f1da0572753ea3b4ade5b6e72e113aa776507fef
Deepak <dbairwa179@gmail.com>
2024-09-27 11:50:41 +0530

    updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -10,8 +10,11 @@ export const Home = ({ toggle }) => {
       </div>
 
       <div className="content">
+        <h1 className="name">Deepak's</h1>
         <h1>Dice Game</h1>
+
         <Button onClick={toggle}>Play now</Button>
+        
       </div>
     </Container>
   );
@@ -26,6 +29,10 @@ const Container = styled.div`
   .content {
     h1 {
       font-size: 96px;
+      white-space: nowrap;
+    }
+    .name{
+      font-size: 85px;
       white-space: nowrap;
     }
   }
